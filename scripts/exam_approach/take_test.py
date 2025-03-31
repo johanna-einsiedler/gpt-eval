bc987dc4325cc4304f5a4318fe3e0d17c6352380
johanna-einsiedler <j.eremita@gmx.at>
2025-03-31 15:24:44 +0200

    add take test

--- scripts/exam_approach/take_test.py
+++ scripts/exam_approach/take_test.py
@@ -10,7 +10,7 @@ import json
 import subprocess
 import shutil
 import google.generativeai as genai
-
+from query_agents import query_agent, take_test
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
@@ -24,154 +24,26 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 test_prompt_template = """Instructions: <instructions> {answer_instructions} </instructions>
 Answer materials: <materials> {answer_materials} </materials>
 Submission instructions: <submission_instructions> {answer_submission} </submission_instructions>"""
-
 system_prompt_template = """You are an expert worker within the domain of {occupation}. Complete the following test."""
-def query_gemini(row, system_prompt_template, test_prompt_template, model='gemini-2.0-flash-thinking-exp', temperature=0):
-    """
-    Queries the Gemini API with a system and user prompt.
-
-    Args:
-        row: A dictionary-like object (e.g., a Pandas Series) containing data for prompt formatting.
-        system_prompt_template: A string template for the system prompt.
-        test_prompt_template: A string template for the user prompt.
-        model: The Gemini model to use (e.g., "gemini-pro").
-        temperature: The temperature parameter for response generation.
-
-    Returns:
-        The generated response as a string, or None if an error occurs.
-    """
-    print("Putting Gemini to the test")
-
-    try:
-        genai.configure(api_key=GOOGLE_API_KEY) # Ensure GOOGLE_API_KEY is defined
-        model_gen = genai.GenerativeModel(model)
-
-        test_prompt = test_prompt_template.format(
-            answer_instructions=row['answer_instructions'],
-            answer_materials=row['answer_materials'],
-            answer_submission=row['answer_submission']
-        )
-
-        system_prompt = system_prompt_template.format(occupation=row['occupation'])
-
-        response = model_gen.generate_content(
-            contents=[system_prompt, test_prompt],
-            generation_config=genai.GenerationConfig(temperature=temperature, max_output_tokens=4096)
-        )
-
-        return response.text
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-def query_deepseek(row,  system_prompt_template, test_prompt_template, model="deepseek-chat", temperature=0):
-    print("Putting DeepSeek to the test")
-    try:
-        client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
-
-        test_prompt = test_prompt_template.format(
-        answer_instructions = row['answer_instructions'],
-        answer_materials = row['answer_materials'],
-        answer_submission = row['answer_submission']
-
-        )
-        
-        message = client.chat.completions.create(
-            model=model,
-            messages=[
-                {"role": "system", "content": system_prompt_template.format(occupation=row['occupation'])},
-                {"role": "user", "content": test_prompt},
-            ],
-            temperature=temperature
-        )
-
-        return message.choices[0].message.content
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-    
-
-
-def query_chatgpt(row, system_prompt_template, test_prompt_template, model="gpt-4o", temperature=0):
-    print("Putting ChatGPT to the test")
-
-    try:
-
-        client = OpenAI(
-            api_key=OPENAI_API_KEY
-        )
-
-        test_prompt = test_prompt_template.format(
-        answer_instructions = row['answer_instructions'],
-        answer_materials = row['answer_materials'],
-        answer_submission = row['answer_submission']
-
-        )
-        
-        message = client.chat.completions.create(
-            messages=[
-                {"role": "developer", "content": system_prompt_template.format(occupation=row['occupation'])},
-                {
-                    "role": "user",
-                    "content": test_prompt
-                }
-            ],
-            model=model,
-            temperature=temperature,
-            max_tokens =4096
-        )
-        return message.choices[0].message.content
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-def query_claude(row, system_prompt_template, test_prompt_template, model="claude-3-7-sonnet-20250219", temperature=0):
-    print("Putting Claude to the test")
-
-    try:
-        client = anthropic.Anthropic(
-        # defaults to os.environ.get("ANTHROPIC_API_KEY")
-        api_key=ANTHROPIC_API_KEY
-        )
-
-        test_prompt = test_prompt_template.format(
-            answer_instructions = row['answer_instructions'],
-            answer_materials = row['answer_materials'],
-            answer_submission = row['answer_submission']
-
-        )
-
-        message = client.messages.create(
-            model=model,
-            max_tokens=8192,
-            system = system_prompt_template.format(occupation=row['occupation']),
-            messages=[
-                {"role": "user", "content": test_prompt}
-            ],
-            temperature=temperature,
-        )
-        out = message.content[0].text
-        return out
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
 
 def save_answer_json(row, path, model):
     folder = os.path.join(path, str(row['task_id']).replace(".", "_"))
     try:
-        answer_json = json.loads(re.search(r'```json(.*?)```', row['test_answers_'+model], re.DOTALL).group(1).strip())
-        print(answer_json)
-    except Exception as e:
-        answer_json='''{"overall_score": 0}'''
-        print('error no valid json')
+        answer_file = json.loads(re.search(r'```json(.*?)```', row['test_answers_'+model], re.DOTALL).group(1).strip())
+    except:
+        try:
+            answer_file =  json.loads(row['test_answers_'+model])
+        except:
+            answer_file = '''{}'''
+            return False
     json_path = os.path.join(path, str(row['task_id']),'/')
     os.makedirs(folder+'/'+model, exist_ok=True)
 
     with open(folder+'/'+model+"/test_submission.json", "w") as json_file:
-        json.dump(answer_json, json_file, ensure_ascii=False, indent=4)
+        json.dump(answer_file, json_file, ensure_ascii=False, indent=4)
 
+    return True
 
 def save_evaluation(row, path):
     folder = os.path.join(path, str(row['task_id']).replace(".", "_").replace("/", "_"))
@@ -180,7 +52,7 @@ def save_evaluation(row, path):
     match = re.search(r'```python(.*?)```', row['answer_grading'], re.DOTALL)
     if not match:
         print(f"Warning: No Python code found in answer_grading for task_id {row['task_id']}")
-        return
+        return False
     
     eval_file = match.group(1).strip()
 
@@ -195,52 +67,62 @@ def save_evaluation(row, path):
         py_file.write(eval_file)
 
     print(f"File saved successfully: {file_path}")
+    return True
+
 
 
 def save_answer_key(row, path):
     folder = os.path.join(path, str(row['task_id']).replace(".", "_"))
-    answer_file = json.loads(re.search(r'```json(.*?)```', row['answer_evaluation'], re.DOTALL).group(1).strip())
+    try:
+        answer_file = json.loads(re.search(r'```json(.*?)```', row['answer_evaluation'], re.DOTALL).group(1).strip())
+    except:
+        try:
+            answer_file =  json.loads(row['answer_evaluation'])
+        except:
+            answer_file = '''{}'''
+            return False
+    
     # Create the folder if it doesn't exist
     os.makedirs(folder, exist_ok=True)
 
     # Write to the Python file
     with open(folder+'/answer_key.json', "w", encoding="utf-8") as json_file:
         json.dump(answer_file, json_file, ensure_ascii=False, indent=4)
+    return True
 
 
 def run_evaluation(row,path, model):
     folder = os.path.join(path, str(row['task_id']).replace('.','_'))
-    print("Running python", folder+"/task_evaluation.py")
-    print("Evaluating ", model)
-    print(os.getcwd())
-    errors =[]
-    try:
-        result = subprocess.run(
-            ["python", "../task_evaluation.py"],
-            cwd=f"{folder}/{model}",  # Use f-string for clarity
-            check=True,  # Raise an exception if the command fails
-            stderr=subprocess.PIPE,  # Capture stderr
-            stdout=subprocess.PIPE   # Capture stdout (if needed)
-        )
-        print("Script executed successfully.")
-        # If the script runs successfully, append None to errors list (no errors)
-        errors.append(None)
-    except subprocess.CalledProcessError as e:
-        # Capture and store the error output in the errors list
-        print(f"Error: Script failed with return code {e.returncode}")
-        print(f"Error Output:\n{e.stderr.decode('utf-8')}")
-        errors.append(e.stderr.decode('utf-8'))  # Append the error message to the errors list
-    except FileNotFoundError:
-        error_message = "Error: The script or directory was not found. Check the path."
-        print(error_message)
-        errors.append(error_message)  # Append the error message to the errors list
-    except Exception as e:
-        # Capture and store any unexpected error
-        error_message = f"An unexpected error occurred: {str(e)}"
-        print(error_message)
-        errors.append(error_message)  # Append the error message to the errors list
-    
-    return errors
+    if row['answer_key_json']* row['evaluation_python']:
+        errors =[]
+        try:
+            result = subprocess.run(
+                ["python", "../task_evaluation.py"],
+                cwd=f"{folder}/{model}",  # Use f-string for clarity
+                check=True,  # Raise an exception if the command fails
+                stderr=subprocess.PIPE,  # Capture stderr
+                stdout=subprocess.PIPE   # Capture stdout (if needed)
+            )
+            print("Script executed successfully.")
+            # If the script runs successfully, append None to errors list (no errors)
+            errors.append(None)
+        except subprocess.CalledProcessError as e:
+            # Capture and store the error output in the errors list
+            print(f"Error: Script failed with return code {e.returncode}")
+            print(f"Error Output:\n{e.stderr.decode('utf-8')}")
+            errors.append(e.stderr.decode('utf-8'))  # Append the error message to the errors list
+        except FileNotFoundError:
+            error_message = "Error: The script or directory was not found. Check the path."
+            print(error_message)
+            errors.append(error_message)  # Append the error message to the errors list
+        except Exception as e:
+            # Capture and store any unexpected error
+            error_message = f"An unexpected error occurred: {str(e)}"
+            print(error_message)
+            errors.append(error_message)  # Append the error message to the errors list   
+        return errors
+    else:
+        return 'no answer key or evaluation script found'
 
   
 
@@ -297,57 +179,86 @@ def collect_overall_scores(row,parent_directory):
     print(scores_dict)
     return scores_dict
 
+
 if __name__ == "__main__":
+    path_to_data = '../../data/exam_approach/exams/'
+    for root, dirs, files in os.walk(path_to_data):
+        for model in dirs:
+            print('Taking the exams of', model)
 
-    # print(os.getcwd())
-    # df = pd.read_csv('../../data/exam_approach/task_prompts/task_prompts_business_and_financial_operations_occupations.csv')
+                 # Create the output directory for the current model if it does not exist
+            output_dir = f'../../data/exam_approach/test_results/{model}/'
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-    # overwrite = False
-    # if ~overwrite:
+            for root, dirs, files in os.walk(path_to_data+model+'/'):
+                for file in files:
+                # Only process CSV files
+                    if file.endswith('.csv'):
+                        file_path = os.path.join(root, file)
+                        print(file_path)
+                        df = pd.read_csv(file_path)
+                        print('Overall ', df.shape[0], ' tasks in the data')
+
+            df= pd.read_csv(f'../../data/exam_approach/test_results/{model}/test_results_business_and_financial_operations_occupations_scores.csv')
+
+            #test models
+            df['test_answers_gemini'] = df.apply(take_test,axis=1, args=(system_prompt_template, test_prompt_template, 'gemini'))
+            df['test_answers_claude'] = df.apply(take_test,axis=1, args=(system_prompt_template, test_prompt_template, 'claude' ))
+            df['test_answers_chatgpt4o'] = df.apply(take_test,axis=1, args=(system_prompt_template, test_prompt_template, "gpt-4o"))
+            df['test_answers_chatgpt35'] = df.apply(take_test,axis=1, args=(system_prompt_template, test_prompt_template,'gpt-3.5-turbo-0125'))
+            df['test_answers_deepseek'] = df.apply(take_test,axis=1, args=(system_prompt_template, test_prompt_template,'deepseek'))
+
+            df.to_csv(f'../../data/exam_approach/test_results/{model}/test_results_business_and_financial_operations_occupations_CORE_automatable.csv')
+
+
+
+
+# overwrite = True
+    # if  overwrite == False:
+    #     print('reading in existing results')
     #     existing_df = pd.read_csv('../../data/exam_approach/test_results/test_results_business_and_financial_operations_occupations.csv')
     #     df = df[~df['task_id'].isin(existing_df['task_id'])]
     # print('Processing ', df.shape[0], ' new tasks.')
   
-    # # test models
-    # df['test_answers_gemini'] = df.apply(query_gemini,axis=1, args=(system_prompt_template, test_prompt_template, ))
-    # df['test_answers_claude'] = df.apply(query_claude,axis=1, args=(system_prompt_template, test_prompt_template, ))
-    # df['test_answers_chatgpt4o'] = df.apply(query_chatgpt,axis=1, args=(system_prompt_template, test_prompt_template,))
-    # df['test_answers_chatgpt35'] = df.apply(query_chatgpt,axis=1, args=(system_prompt_template, test_prompt_template,'gpt-3.5-turbo-0125'))
-    # df['test_answers_deepseek'] = df.apply(query_deepseek,axis=1, args=(system_prompt_template, test_prompt_template,))
+        # #
+    
+    # # # read in labels & filter exposure 
+   
+            # print(df.shape[0])
+            model_folder_path = f'../../data/exam_approach/test_results/{model}/'
+            # save answers as json files
+            print('saving answer files')
+            df['answer_valid_chatgpt4o'] = df.apply(save_answer_json, axis=1, args=(model_folder_path, 'chatgpt4o'))
+            df['answer_valid_chatgpt35'] = df.apply(save_answer_json, axis=1, args=(model_folder_path, 'chatgpt35'))
+            df['answer_valid_deepseek'] = df.apply(save_answer_json, axis=1, args=(model_folder_path, 'deepseek'))
+            df['answer_valid_claude'] = df.apply(save_answer_json, axis=1, args=(model_folder_path, 'claude'))
+            df['answer_valid_gemini'] = df.apply(save_answer_json, axis=1, args=(model_folder_path, 'gemini'))
+            df.to_csv(f'../../data/exam_approach/test_results/{model}/test_results_business_and_financial_operations_occupations_scores.csv')
 
-    #df.to_csv('../../data/exam_approach/test_results/test_results_business_and_financial_operations_occupations.csv')
-    df = pd.read_csv('../../data/exam_approach/test_results/test_results_business_and_financial_operations_occupations.csv')
-    print(df.shape[0])
-    # # save answers as json files
-    # print('saving answer files')
-    # df.apply(save_answer_json, axis=1, args=('../../data/exam_approach/test_results/', 'chatgpt4o'))
-    # df.apply(save_answer_json, axis=1, args=('../../data/exam_approach/test_results/', 'chatgpt35'))
-    # df.apply(save_answer_json, axis=1, args=('../../data/exam_approach/test_results/', 'deepseek'))
-    # df.apply(save_answer_json, axis=1, args=('../../data/exam_approach/test_results/', 'claude'))
-    # df.apply(save_answer_json, axis=1, args=('../../data/exam_approach/test_results/', 'gemini'))
+            print('saving evaluation files and answer keys')
+            # save evaluation script and answer key in appropriate folders
+            df['evaluation_python'] = df.apply(save_evaluation, axis=1, args=(model_folder_path,))
+            df['answer_key_json']=  df.apply(save_answer_key, axis=1, args=(model_folder_path,))
+            df.apply(copy_answer_key, axis=1, args=(model_folder_path,))
 
-    # print('saving evaluation files and answer keys')
-    # # save evaluation script and answer key in appropriate folders
-    # df.apply(save_evaluation, axis=1, args=('../../data/exam_approach/test_results/',))
-    # df.apply(save_answer_key, axis=1, args=('../../data/exam_approach/test_results/',))
-    # df.apply(copy_answer_key, axis=1, args=('../../data/exam_approach/test_results/',))
 
-    # # run evaluation and document potential errors
-    # print('running evaluation')
-    df['errors_chatgpt35'] = df.apply(run_evaluation, axis=1, args=('../../data/exam_approach/test_results/','chatgpt35',))
-    df['errors_chatgpt4o'] = df.apply(run_evaluation, axis=1, args=('../../data/exam_approach/test_results/','chatgpt4o',))
-    df['errors_claude'] = df.apply(run_evaluation, axis=1, args=('../../data/exam_approach/test_results/','claude',))
-    df['errors_deepseek'] = df.apply(run_evaluation, axis=1, args=('../../data/exam_approach/test_results/','deepseek',))
-    df['errors_gemini'] = df.apply(run_evaluation, axis=1, args=('../../data/exam_approach/test_results/','gemini',))
+            # run evaluation and document potential errors
+            print('running evaluation')
+            df['errors_chatgpt35'] = df.apply(run_evaluation, axis=1, args=(model_folder_path,'chatgpt35',))
+            df['errors_chatgpt4o'] = df.apply(run_evaluation, axis=1, args=(model_folder_path,'chatgpt4o',))
+            df['errors_claude'] = df.apply(run_evaluation, axis=1, args=(model_folder_path,'claude',))
+            df['errors_deepseek'] = df.apply(run_evaluation, axis=1, args=(model_folder_path,'deepseek',))
+            df['errors_gemini'] = df.apply(run_evaluation, axis=1, args=(model_folder_path,'gemini',))
 
-    # collect exam scores
-    print('collecting scores')
-    df['scores'] =  df.apply(collect_overall_scores,axis=1, args= ('../../data/exam_approach/test_results/',))
-    scores_df = pd.json_normalize(df['scores'])
-    scores_df.columns = 'score_'+scores_df.columns
-    # Combine the original DataFrame with the new columns
-    df_expanded = pd.concat([df.drop('scores', axis=1), scores_df], axis=1)
-    scores_df.to_csv('../../data/exam_approach/test_results/overall_scores.csv')
+            print('collecting scores')
+            df['scores'] =  df.apply(collect_overall_scores,axis=1, args= (model_folder_path,))
+            scores_df = pd.json_normalize(df['scores'])
+            scores_df.columns = 'score_'+scores_df.columns
+            # Combine the original DataFrame with the new columns
+            df_expanded = pd.concat([df.drop('scores', axis=1), scores_df], axis=1)
+            
+            df_expanded.to_csv(f'../../data/exam_approach/test_results/{model}/test_results_business_and_financial_operations_occupations_scores.csv')
 
-    df_expanded.to_csv('../../data/exam_approach/test_results/test_results_business_and_financial_operations_occupations_scores.csv')
-    #
+            scores_df = pd.concat([df[['task_id','occupation','task_description']],scores_df], axis=1)
+            scores_df.to_csv(f'../../data/exam_approach/test_results/{model}/overall_scores.csv')
